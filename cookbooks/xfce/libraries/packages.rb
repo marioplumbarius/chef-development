276710c83a549ad11878fbc268e0f3197112de90
mario.souza <mario.souza@catho.com>
2017-05-31 10:10:57 -0300

    split packages into groups for xfce recipe

--- cookbooks/xfce/libraries/packages.rb
+++ cookbooks/xfce/libraries/packages.rb
@@ -1,25 +1,31 @@
 module PACKAGES
 
-    ALL = [
-        'xfce4-appfinder',
+    PLUGINS = [
         'xfce4-cpufreq-plugin',
         'xfce4-cpugraph-plugin',
         'xfce4-genmon-plugin',
         'xfce4-netload-plugin',
+        'xfce4-pulseaudio-plugin',
+        'xfce4-sensors-plugin',
+        'xfce4-systemload-plugin',
+    ]
+
+    GENERAL = [
+        'xfce4-appfinder',
         'xfce4-panel',
         'xfce4-power-manager',
-        'xfce4-pulseaudio-plugin',
         'xfce4-screenshooter',
-        'xfce4-sensors-plugin',
         'xfce4-session',
         'xfce4-session-engines',
         'xfce4-settings',
-        'xfce4-systemload-plugin',
         'xfce4-terminal',
         'xfce-polkit',
         'xscreensaver',
-        'xfce4-mixer',
-        'pavucontrol'
+        'pavucontrol',
+    ]
+
+    ALL = [
+        PLUGINS, GENERAL
     ]
 
 end
